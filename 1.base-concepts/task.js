cfd9204e9920237b577895047af37c969366013b
Cen-2-rion <147451031+Cen-2-rion@users.noreply.github.com>
2023-11-18 10:26:40 +0500

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,20 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let d = Math.pow(b, 2) - 4 * a * c;
-  
-	if (d === 0) {
-	  arr.push(-b / (2 * a));
-	}
-	else if (d > 0) {
-	  arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
-	}
-	return arr;
-  }
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
 
-  function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let rateRange = percent / 100 / 12;
-	let payment = (amount - contribution) * (rateRange + (rateRange / (Math.pow((1 + rateRange), countMonths) - 1))) * countMonths;
-	return +payment.toFixed(2);
+  if (d === 0) {
+    arr.push(-b / (2 * a));
   }
-  
+  else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+  }
+  return arr;
+}
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let rateRange = percent / 100 / 12;
+  let payment = (amount - contribution) * (rateRange + (rateRange / (Math.pow((1 + rateRange), countMonths) - 1))) * countMonths;
+  return +payment.toFixed(2);
+}
