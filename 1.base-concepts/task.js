8dc460ce9e28999e5b7ed9358feb29915371e747
Aleksandr Atrohov <atrohov_82@mail.ru>
2023-11-18 10:15:49 +0500

    Первое задание доработка

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,41 +1,21 @@
 "use strict"
 
-// Task №1
-
 function solveEquation(a, b, c) {
 	let arr = [];
 	let d = Math.pow(b, 2) - 4 * a * c;
-	console.log('Дискриминант = ' + d);
-	if (d < 0) {
-		console.log('Корней нет');
-	}
+  
 	if (d === 0) {
-		console.log('Квадратный корень уравнения = ' + (-b / (2 * a)));
-	} else if (d > 0) {
-		let equationRoots = [];
-		equationRoots.push((-b + Math.sqrt(d)) / (2 * a) + ' и ' + (-b - Math.sqrt(d)) / (2 * a));
-		console.log('Квадратные корни уравнения = ' + equationRoots);
+	  arr.push(-b / (2 * a));
+	}
+	else if (d > 0) {
+	  arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
 	}
 	return arr;
-}
-console.log(solveEquation(1, 5, 4));
+  }
 
-// Task №2
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	percent = percent / 100 / 12; // преобразование диапазона ежемесячной процентной ставки
-	payment = (amount - contribution) * (percent + (percent / (Math.pow((1 + percent), countMonths) - 1)));
-	return payment.toFixed(2);
-}
-
-amount = 50000, // сумма кредита
-	contribution = 0, // первоначальный взнос
-	percent = 10, // процентная ставка
-	countMonths = 12; // срок кредита в месяцах
-
-console.log("Общая сумма кредита: " + amount + " руб");
-console.log("Взнос: " + contribution + " руб");
-console.log("Ставка: " + percent + " %");
-console.log("Срок кредита: " + countMonths + " месяцев");
-console.log("Ежемесячный платеж: " + calculateTotalMortgage(percent, contribution, amount, countMonths) + " руб");
-console.log("Общяя сумма платежа: " + (payment * countMonths).toFixed(2) + " руб");
+  function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	let rateRange = percent / 100 / 12;
+	let payment = (amount - contribution) * (rateRange + (rateRange / (Math.pow((1 + rateRange), countMonths) - 1))) * countMonths;
+	return +payment.toFixed(2);
+  }
+  
