1cc4e4917b33c95c3d447023b5c3043a09611632
Aleksandr Atrohov <atrohov_82@mail.ru>
2023-11-25 18:27:20 +0500

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((e, i) => e === arr2[i] && arr1.length === arr2.length);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    let sumCount = users.filter(item => item.gender === gender).reduce((acc, item) => acc + item.age, 0);
+    let filteredCount = users.filter(item => item.gender === gender, 0);
+    if (filteredCount.length == 0) {
+      return 0;
+    }
+    else {
+      return sumCount / filteredCount.length;
+    }
+  }
   
-}
