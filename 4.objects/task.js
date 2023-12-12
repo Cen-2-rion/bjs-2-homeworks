51d2dff41645180577fedfae25a2cf46bbe65f88
Aleksandr Atrohov <atrohov_82@mail.ru>
2023-12-12 22:54:45 +0500

    Update

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,33 +2,33 @@ function Student(name, gender, age) {
 	this.name = name;
 	this.gender = gender;
 	this.age = age;
-}
-
-let student1 = new Student("Джоэл", "мужской", 25);
-let student2 = new Student("Элли", "женский", 19);
-
-Student.prototype.setSubject = function(subjectName) {
+	this.marks = [];
+  }
+  
+  let student1 = new Student('Джоэл', 'мужской', 25);
+  let student2 = new Student('Элли', 'женский', 19);
+  
+  Student.prototype.setSubject = function (subjectName) {
 	this.subject = subjectName;
-}
-
-Student.prototype.addMarks = function(...marks) {
-	if (this.marks === undefined) {
-		this.marks = marks;
-	} else {
-		this.marks = this.marks.concat(marks);
+  };
+  
+  Student.prototype.addMarks = function (...marks) {
+	if (this.hasOwnProperty("marks")) {
+	  this.marks.push(...marks);
 	}
-}
-
-Student.prototype.getAverage = function() {
-	if (this.marks === undefined) {
-		return 0;
+  };
+  
+  Student.prototype.getAverage = function () {
+	if (this.hasOwnProperty("marks") && this.marks !== []) {
+	  return this.marks.reduce((acc, value) => acc + value / this.marks.length, 0);
 	} else {
-		return this.marks.reduce((acc, value) => acc + value, 0) / this.marks.length;
+	  return 0;
 	}
-}
-
-Student.prototype.exclude = function(reason) {
+  };
+  
+  Student.prototype.exclude = function (reason) {
 	delete this.subject;
 	delete this.marks;
 	this.excluded = reason;
-}
+  };
+  
