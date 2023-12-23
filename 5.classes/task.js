d2581f340e00c67f28e4e03e673173acfb197ac1
Aleksandr Atrohov <atrohov_82@mail.ru>
2023-12-24 00:57:29 +0500

    Update

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -11,7 +11,7 @@ class PrintEditionItem {
     }
     set state (number) {
         if (number < 0) {
-            this.state = 0;
+            this._state = 0;
         }
         else if (number > 100) {
             this._state = 100;
@@ -42,24 +42,21 @@ class Book extends PrintEditionItem {
 
 class NovelBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.author = author;
+        super(author, name, releaseDate, pagesCount);
         this.type = 'novel';
     }
 }
 
 class FantasticBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.author = author;
+        super(author, name, releaseDate, pagesCount);
         this.type = 'fantastic';
     }
 }
 
 class DetectiveBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.author = author;
+        super(author, name, releaseDate, pagesCount);
         this.type = 'detective';
     }
 }
