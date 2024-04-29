a990e9b342fbbc1cf788618a0ae6e498d13d6348
Aleksandr Atrohov <atrohov_82@mail.ru>
2024-04-29 08:14:43 +0500

    first commit decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,50 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    const maxCacheValuesCount = 5;
+    
+    return (...args) => {
+        const hash = md5(args);
+        let objectInCache = cache.find((object) => object.hash === hash);
+    
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.value);
+            return "Из кеша: " + objectInCache.value;
+        }
+        let result = func(...args);
+        cache.push({hash: hash, value: result});
+    
+        if (cache.length > maxCacheValuesCount) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
 }
+
+const hasingText = "какой-нибудь текст";
+console.log(md5(hasingText)); // 8d1d3ecc455a4220590e6d27e6c1a267
+console.log(md5([10, 20, 30])); // 7f49b84d0bbc38e96493718013baace6
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
   
+    function arc(...args) {
+      clearTimeout(timeoutId);
+      func.apply(this, [...args]);
+      wrapper.count++;
+    }
+  
+    function wrapper(...args) {
+      if(!timeoutId) {
+        arc(args);
+      }
+      timeoutId = setTimeout(() => arc(args), delay);
+      wrapper.allCount++;
+    }
+  
+    return wrapper;
 }
